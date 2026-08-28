ca2082619fca75e7b6e4b1bf2210d0ef3e43c79b
Mulyawan <fr1301@protonmail.com>
2026-08-28 13:00:44 +0700

    feat: create push feature

---
+++ gopress.go
@@ -0,0 +1,18 @@
+package main
+
+type GoPress struct {
+	queue chan string
+}
+
+func NewGoPress(cap int) *GoPress {
+	return &GoPress{queue: make(chan string, cap)}
+}
+
+func (p *GoPress) Push(item string) bool {
+	select {
+	case p.queue <- item:
+		return true
+	default:
+		return false
+	}
+}
