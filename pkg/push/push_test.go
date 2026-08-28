3cb4ae8a719ff6e3440a184fce321f95c76e3343
Mulyawan <fr1301@protonmail.com>
2026-08-28 13:35:55 +0700

    feat: add counter to push test

--- pkg/push/push_test.go
+++ pkg/push/push_test.go
@@ -1,14 +1,23 @@
 package push
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/Mulyawan-ts/gopress/pkg/counter"
+)
 
 func TestPush(t *testing.T) {
 	q := make(chan string, 1)
+	c := counter.New()
 	p := NewPusher(q)
 
 	p.Push("A")
 
 	if p.Push("B") {
 		t.Error("B harusnya ditolak karena channel penuh")
+	}
+
+	if got := c.Value(); got != 1 {
+		t.Errorf("Harusnya dropped count = 1, tapi dapat %d", got)
 	}
 }
