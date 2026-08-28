16ecfd76558ad6f970799192b856f2daecc6f712
Mulyawan <fr1301@protonmail.com>
2026-08-28 13:08:21 +0700

    feat: update gopress test

--- pkg/gopress/gopress_test.go
+++ pkg/gopress/gopress_test.go
@@ -1,17 +1,12 @@
-package main
+package gopress
 
 import "testing"
 
 func TestPush(t *testing.T) {
-	p := NewGoPress(1) // Kapasitas 1
+	p := New(1)
+	p.Push("A")
 
-	// Test 1: Masuk antrean (harus true)
-	if ok := p.Push("A"); !ok {
-		t.Error("Harusnya A berhasil masuk")
-	}
-
-	// Test 2: Antrean penuh (harus false)
-	if ok := p.Push("B"); ok {
-		t.Error("Harusnya B ditolak")
+	if p.Push("B") {
+		t.Error("B ditolak karena antrean full")
 	}
 }
